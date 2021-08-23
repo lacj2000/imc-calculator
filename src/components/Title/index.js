9b7e72ef761c78c87986b50d411e0c6ff2210418
Luiz Júnior <41206546+lacj2000@users.noreply.github.com>
2021-08-23 10:53:41 -0300

    add - component title

---
+++ src/components/Title/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+function Title(){
+    return (
+        <View>
+            <Text>IMC CALCULATOR</Text>
+        </View>
+    );
+}
+
+export default Title;
